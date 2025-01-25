f3dbf8d9ee083c586f59241b3a1f845a75221cff
kateleext <83829378+kateleext@users.noreply.github.com>
2025-01-25 00:02:44 +0000

    layout

--- dice.rb
+++ dice.rb
@@ -9,15 +9,15 @@ use(BetterErrors::Middleware)
 BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
-get("/") do
+get('/') do
   erb(:home)
 end
-
 
 get("/dice/2/6") do
   first_die = rand(1..6)
 	second_die = rand(1..6)
   sum = first_die + second_die
+
 	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
   erb(:two_six)
 end
@@ -27,19 +27,17 @@ get("/dice/2/10") do
 	second_die = rand(1..10)
   sum = first_die + second_die
 	
-	outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-	"<h1>2d10</h1>
-   <p>#{outcome}</p>"
+	erb(:two_ten)
 end
 
 get("/dice/1/20") do
   die = rand(1..20)
 	
-	outcome = "You rolled a #{die}."
+	@outcome = "You rolled a #{die}."
 	
-	"<h1>1d20</h1>
-   <p>#{outcome}</p>"
+	erb(:one_twenty)
 end
 
 get("/dice/5/4") do
@@ -49,8 +47,7 @@ get("/dice/5/4") do
   fourth_die = rand(1..4)
   sum = first_die + second_die + third_die + fourth_die
 	
-	outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
+	@outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
 	
-	"<h1>5d4</h1>
-   <p>#{outcome}</p>"
+  erb(:five_four)
 end
